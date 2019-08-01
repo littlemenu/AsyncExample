e532318bf9b470c584604db09d18094f6fa96a17
littlemenu <jjhisking@gmail.com>
2019-08-01 11:58:24 +0900

    Using OperationQueue

--- AsyncExample/ViewController.swift
+++ AsyncExample/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpDownloadButton(_ sender: UIButton) {
+        // 이미지 다운로드 -> 이미지 뷰에 셋팅
+        // https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg
+        
+        guard let imageURL: URL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg") else {
+            return
+        }
+        
+        OperationQueue().addOperation {
+            let imageData: Data = try! Data.init(contentsOf: imageURL)
+            guard let image: UIImage = UIImage(data: imageData) else {
+                return
+            }
+            
+            OperationQueue.main.addOperation {
+                self.imageView.image = image
+            }
+        }
+    }
 }
-
